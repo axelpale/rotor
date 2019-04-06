0629f44646b80d3b72291112ffb7c25a5b0e8a01
Akseli Palen <akseli.palen@gmail.com>
2019-04-06 13:09:24 +0300

    Init v2015-03-24

---
+++ data.php
@@ -0,0 +1,37 @@
+<?php
+   // Page data
+   // Possible fields are:
+   //  title
+   //  content
+   //  template
+   $data = array(
+      1 => array(
+         "title" => "Aztecstry",
+         "name" => "01_aztecstry"
+         ),
+      2 => array(
+         "title" => "Gaia",
+         "name" => "02_gaia"
+         ),
+      3 => array(
+         "title" => "Construction",
+         "name" => "03_construction"
+         ),
+      4 => array(
+         "title" => "Stems",
+         "name" => "04_stems"
+         ),
+      5 => array(
+         "title" => "Witchdrum",
+         "name" => "05_witchdrum"
+       ),
+      6 => array(
+         "title" => "Shelter",
+         "name" => "06_shelter"
+       ),
+      7 => array(
+         "title" => "Jaw",
+         "name" => "07_jaw",
+         )
+   );
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+   // Get header
+   require_once("header.php");
+   
+   // Get content
+   include_once( "page.php" );
+   
+   // Get footer
+   require_once("footer.php");
+?>
